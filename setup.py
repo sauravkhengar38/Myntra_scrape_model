fcc80622ad0b2847d87039b2167016cd0ca0cf2f
Saurav <sauravkhengar07@gmail.com>
2025-06-05 21:01:34 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'scrapper',
+    version = '0.0.1',
+    author = 'pwskills',
+    author_email='sk.professional38@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+    )
